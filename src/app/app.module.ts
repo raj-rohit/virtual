2325d83435559e78c81da67f83c9a330717d5c3c
Raj <rohit.i.raj@accenture.com>
2018-02-25 21:27:50 +0530

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { StudentComponent } from './student/student.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { NewuserComponent } from './newuser/newuser.component';
 
+const appRoutes: Routes = [
+  { path: 'stud', component: StudentComponent },
+  { path: 'teach', component: TeacherComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'newuser', component: NewuserComponent},
+  { path: '', component: LoginComponent }
+  
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeacherComponent,
+    StudentComponent,
+    AdminComponent,
+    LoginComponent,
+    NewuserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router }  from '@angular/router'; 
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent  {
+
+  username:string;password:string;
+  constructor(private _router: Router){} 
+
+  onSubmit(){ 
+    console.log("I am healthy");
+    if(this.username=="admin" && this.password=="cleopatra")
+    {
+    console.log("I am healthy");
+     this._router.navigate(['/admin']); 
+  }
+  else if(this.username=='student' && this.password=="manorama")
+  {
+    console.log("I am healthy");
+     this._router.navigate(['/stud']); 
+  }
+  else if(this.username=='teacher' && this.password=="monopoly")
+  {
+    console.log("I am healthy");
+     this._router.navigate(['/teach']); 
+  }
+
+  }
+
+}
