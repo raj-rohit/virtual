8785a900ee2fec3e4e10d169f8ca2a2871d69b77
Raj <rohit.i.raj@accenture.com>
2018-03-09 15:09:14 +0530

    Firebase Database Conectivyt done

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [AngularFireDatabase]
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  itemValue = '';
+  items: FirebaseListObservable<any[]>;
+ 
+  constructor(db: AngularFireDatabase) {
+    this.items = db.list('/items');
+  }
 
+  onSubmit() {
+    this.items.push({content: this.itemValue});
+    this.itemValue = '';
+  }
   ngOnInit() {
   }
 
