6b694ee1667bad0d829eaeb6e13574251a239f0b
Raj <rohit.i.raj@accenture.com>
2018-03-11 14:34:19 +0530

    Chat

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
 
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
-  styleUrls: ['./student.component.css']
+  styleUrls: ['./student.component.css'],
+  providers: [AngularFireDatabase]
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+  itemValue = '';
+  items: FirebaseListObservable<any[]>;
+ 
+  constructor(db: AngularFireDatabase) {
+    this.items = db.list('/items');
+  }
+
+  onSubmit() {
+    this.items.push({content: "Student "+this.itemValue});
+    this.itemValue = '';
+  }
 
   ngOnInit() {
   }
